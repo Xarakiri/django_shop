4b7cd4bc451736d3f28e928cef9e1257d060feac
Xarakiri <rafikov.ds777@gmail.com>
2021-07-23 20:04:33 +0500

    add category model

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+#**************
+#1 Category
+#2 Product
+#3 CartProduct
+#4 Cart
+#5 Order
+#**************
+#6 Customer
+#7 Specification
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Имя категории')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
