92d80fbaff4cf0363a923aa9fc3069086376a748
Xarakiri <rafikov.ds777@gmail.com>
2021-07-23 20:08:06 +0500

    add product model

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -18,3 +18,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Изображение')
+    description = models.TextField(verbose_name='Описание', null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.title
+
