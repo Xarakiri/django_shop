2a8fce2d551ccdb35d9adb9e0fbfa0e88f9a6dbc
Xarakiri <rafikov.ds777@gmail.com>
2021-07-23 21:46:14 +0500

    register models

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+admin.site.register(Category)
+admin.site.register(Notebook)
+admin.site.register(Smartphone)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
+
