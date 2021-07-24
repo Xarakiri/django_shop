0764f762b78c0ecd05280ed72c4fdc902741637d
Xarakiri <rafikov.ds777@gmail.com>
2021-07-24 22:44:37 +0500

    some fixes for smartphone sd

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -133,8 +133,8 @@ class Smartphone(Product):
     resolution = models.CharField(max_length=255, verbose_name='Разрешение экрана')
     accum_volume = models.CharField(max_length=255, verbose_name='Объем батареи')
     ram = models.CharField(max_length=255, verbose_name='Оперативная память')
-    sd = models.BooleanField(default=True)
-    sd_volume_max = models.CharField(max_length=255, verbose_name='Максимальный объе встраиваемой памяти')
+    sd = models.BooleanField(default=True, verbose_name='Наличие SD карты')
+    sd_volume_max = models.CharField(max_length=255, null=True, blank=True, verbose_name='Максимальный объе встраиваемой памяти')
     main_camp_mp = models.CharField(max_length=255, verbose_name='Главная камера')
     frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальная камера')
 
