f1038f942f384360ff32d6c0071e6e4f0d588e3e
Xarakiri <rafikov.ds777@gmail.com>
2021-07-24 23:56:24 +0500

    add mixin for category

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 from .models import Notebook, Smartphone, Category
+from .mixins import CategoryDetailMixin
 
 
-def test_view(request):
-    categories = Category.objects.get_categories_for_nav_bar()
-    return render(request, 'base.html', {'categories': categories})
+class BaseView(View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_nav_bar()
+        return render(request, 'base.html', {'categories': categories})
+
+# def test_view(request):
+#     categories = Category.objects.get_categories_for_nav_bar()
+#     return render(request, 'base.html', {'categories': categories})
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(CategoryDetailMixin, DetailView):
 
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook, 
@@ -25,11 +32,10 @@ class ProductDetailView(DetailView):
     slug_url_kwarg = 'slug'
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(CategoryDetailMixin, DetailView):
 
     model = Category
     queryset = Category.objects.all()
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
-    
