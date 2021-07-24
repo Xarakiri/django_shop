b49ac1641866c98cd2c037e4a2fcdd31b2b6404b
Xarakiri <rafikov.ds777@gmail.com>
2021-07-24 13:15:53 +0500

    temporary remove image validation

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -70,28 +70,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
-    
-    def save(self, *args, **kwargs):
-        # image = self.image
-        # img = Image.open(image)
-        # min_height, min_width = self.MIN_RESOLUTION
-        # max_height, max_width = self.MAX_RESOLUTION
-        # if img.height < min_height or img.width < min_width:
-        #     raise MinResolutionErrorException('Разрешение изображения меньше минимального')
-        # if img.height > max_height or img.width > max_width:
-        #     raise MaxResolutionErrorException('Разрешение изображения больше максимального')
-        image = self.image
-        img = Image.open(image)
-        new_img = img.convert('RGB')
-        resized_new_img = new_img.resize((200, 200), Image.ANTIALIAS)
-        filestream = BytesIO()
-        resized_new_img.save(filestream, 'JPEG', quality=90)
-        filestream.seek(0)
-        name = '{}.{}'.format(*self.image.name.split('.'))
-        self.image = InMemoryUploadedFile(
-            filestream, 'ImageField', name, 'jpeg/image', sys.getsizeof(filestream), None
-        )
-        super().save(*args, **kwargs)
 
 
 class CartProduct(models.Model):
