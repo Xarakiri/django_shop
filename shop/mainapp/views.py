105d1637953c09d95f45be95cb643ff2c0b510f9
Xarakiri <rafikov.ds777@gmail.com>
2021-07-24 13:55:17 +0500

    extend from base.html

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -19,8 +19,6 @@ class ProductDetailView(DetailView):
         self.queryset = self.model._base_manager.all()
         return super().dispatch(request, *args, **kwargs)
 
-    # model = Model
-    # queryset = Models.objects.all()
     context_object_name = 'product'
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
