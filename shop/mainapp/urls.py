e7b5cc3214d5e2845a4d842f885671030edcd31f
Xarakiri <rafikov.ds777@gmail.com>
2021-07-25 17:14:00 +0500

    implement add to cart logic

--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import BaseView, ProductDetailView, CategoryDetailView, CartView
+from .views import BaseView, ProductDetailView, CategoryDetailView, CartView, AddToCartView
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('category/<str:slug>', CategoryDetailView.as_view(), name='category_detail'),
     path('cart/', CartView.as_view(), name='cart'),
+    path('add-to-cart/<str:ct_model>/<str:slug>/', AddToCartView.as_view(), name='add_to_cart'),
 ]
