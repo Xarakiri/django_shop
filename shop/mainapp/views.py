561f4707231ea941c234fa6d386aef8fb887c346
Xarakiri <rafikov.ds777@gmail.com>
2021-07-30 21:41:02 +0500

    add order view and fix model

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -139,3 +139,22 @@ class CheckoutView(CartMixin, View):
             'form': form,
         }
         return render(request, 'checkout.html', context)
+
+
+class MakeOrderView(CartMixin, View):
+
+    def post(self, request, *args, **kwargs):
+        form = OrderForm(request.POST or None)
+        if form.is_valid():
+            new_order = form.save(commit=False)
+            new_order.customer = Customer.objects.get(user=request.user)
+            new_order.first_name = form.cleaned_data['first_name']
+            new_order.last_name = form.cleaned_data['last_name']
+            new_order.phone = form.cleaned_data['phone']
+            new_order.address = form.cleaned_data['address']
+            new_order.buying_type = form.cleaned_data['buying_type']
+            new_order.order_date = form.cleaned_data['order_date']
+            new_order.comment = form.cleaned_data['comment']
+            new_order.save()
+            self.cart.in_order = True
+        
